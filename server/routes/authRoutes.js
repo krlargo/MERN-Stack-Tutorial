6b1316eece0fc3d7b54ad45c6fa04135709e9eed
krlargo <krlargo@ucdavis.edu>
2018-01-05 17:54:44 -0800

    Move RouteHandler methods into routes directory

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,13 @@
+const passport = require('passport');
+
+// Export app methods from this file
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+};
