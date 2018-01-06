38901b5d7b1db99f8d33c908f02f9f4e953d310c
krlargo <krlargo@ucdavis.edu>
2018-01-05 17:55:31 -0800

    Refactor code by splitting into separate files

--- server/index.js
+++ server/index.js
@@ -1,36 +1,7 @@
-// How we import through server-side since node doesn't support 'import'
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport'); // Not assigned to a variable; simply needs to be called
 
 const app = express();
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
-      console.log('done', done);
-    }
-  )
-);
-
-// Route Handler
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 // Look at underlying environment and see if a PORT is declared
 const PORT = process.env.PORT || 5000; // 5000 is default value in case of null
