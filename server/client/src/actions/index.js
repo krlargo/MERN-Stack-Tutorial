d6edc416c7e2f96230749a27797e5a5d26230440
krlargo <krlargo@ucdavis.edu>
2018-01-08 00:55:07 -0800

    Refactor using async/await and fat arrow funcs

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,15 +1,10 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  // ReduxThunk pays attention to whether we pass back an action or a function
-  // - if we pass a function, dispatch is automatically passed to ActionCreator
-  return function(dispatch) {
-    axios.get('/api/current_user').then(res =>
-      dispatch({
-        type: FETCH_USER,
-        payload: res
-      })
-    );
-  };
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('api/current_user');
+  dispatch({
+    type: FETCH_USER,
+    payload: res
+  });
 };
