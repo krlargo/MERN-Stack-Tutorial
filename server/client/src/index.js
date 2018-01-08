012a37c9c48e1ad8e914b18d7550f4dcb9bd746d
krlargo <krlargo@ucdavis.edu>
2018-01-07 23:36:13 -0800

    Import MaterializeCSS

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,7 +1,10 @@
+// When importing .css file, we don't have any assignment, hence lack of 'VAR from'
+import 'materialize-css/dist/css/materalize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from './components/App';
 import reducers from './reducers';
@@ -11,7 +14,9 @@ const store = createStore(reducers, {}, applyMiddleware());
 // Provider is responsible for alerting its child components of store's state changes
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <div>
+      <App />
+    </div>
   </Provider>,
   document.querySelector('#root')
 );
