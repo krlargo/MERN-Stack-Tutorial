c183efc72b3dc9ce3a58576a1b5158d2e7920421
krlargo <krlargo@ucdavis.edu>
2018-01-08 00:43:50 -0800

    Connect actions to App.js via react-redux.connect

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SuveryNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  // MaterializeCSS uses (at least one) className="container" to estimate sizing
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Components {
+  componentDidMount() {
+    const user = this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    // MaterializeCSS uses (at least one) className="container" to estimate sizing
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
