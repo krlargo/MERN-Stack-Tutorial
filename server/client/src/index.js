2ccc57d51c2270c40f62b964c8aaedc03085ea11
krlargo <krlargo@ucdavis.edu>
2018-01-08 00:44:23 -0800

    Import ReduxThunk

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -5,11 +5,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+import actions from './actions';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 // Provider is responsible for alerting its child components of store's state changes
 ReactDOM.render(
