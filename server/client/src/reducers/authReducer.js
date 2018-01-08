c88db136521a590624dde51d7ddd3524ea5486c6
krlargo <krlargo@ucdavis.edu>
2018-01-08 00:44:59 -0800

    Add console.log to check user auth

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,4 +1,8 @@
+import { FETCH_USER } from '../actions/types';
+
 export default function(state = {}, action) {
+  console.log(action);
+
   switch (action.type) {
     default:
       return state;
