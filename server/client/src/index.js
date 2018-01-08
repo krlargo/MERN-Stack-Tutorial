98add033aeae70e613a4cee2d814becdee097a07
krlargo <krlargo@ucdavis.edu>
2018-01-07 21:27:05 -0800

    Fix typos

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.renderDOM.render();
+import App from './components/App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
